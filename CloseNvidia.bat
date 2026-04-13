19e637abe2b23f2d55b055c3c4c425012d5ba00a
Guidsat <guilhermereis564@gmail.com>
2026-04-13 16:03:36 -0300

    Update CloseNvidia.bat

--- CloseNvidia.bat
+++ CloseNvidia.bat
@@ -1,18 +1,28 @@
 @echo off
-taskkill /f /im "NVIDIA Overlay.exe"
+taskkill /f /im "NVIDIA Overlay.exe" >nul 2>&1
 
 
-set "SHORTCUT_PATH=%USERPROFILE%\Desktop\KillNVIDIA.lnk"
-
-if not exist "%SHORTCUT_PATH%" (
-    set "SCRIPT_FULL_PATH=%~f0"
-    set "FOLDER_PATH=%~dp0"
-    set "ICON_PATH=%FOLDER_PATH%nvidia_icon.ico"
-    
-    powershell -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%SHORTCUT_PATH%');$s.TargetPath='%SCRIPT_FULL_PATH%';$s.IconLocation='%ICON_PATH%';$s.Save()"
-    
-    echo Desktop shortcut created successfully.
+if exist "%USERPROFILE%\OneDrive\Desktop" (
+    set "DESKTOP_PATH=%USERPROFILE%\OneDrive\Desktop"
+) else (
+    set "DESKTOP_PATH=%USERPROFILE%\Desktop"
 )
 
-echo NVIDIA Overlay closed.
+set "SHORTCUT_PATH=%DESKTOP_PATH%\KillNVIDIA.lnk"
+set "SCRIPT_FULL_PATH=%~f0"
+set "FOLDER_PATH=%~dp0"
+set "ICON_PATH=%FOLDER_PATH%nvidia_icon.ico"
+
+if not exist "%SHORTCUT_PATH%" (
+    powershell -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%SHORTCUT_PATH%');$s.TargetPath='%SCRIPT_FULL_PATH%';$s.IconLocation='%ICON_PATH%';$s.Save()"
+    if exist "%SHORTCUT_PATH%" (
+        echo Desktop shortcut created successfully!
+    ) else (
+        echo [!] Failed to create shortcut. Check permissions.
+    )
+)
+
+echo.
+echo NVIDIA Overlay process terminated.
+echo.
 pause
